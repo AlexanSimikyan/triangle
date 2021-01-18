4903287ecd0f994e2f1e8b7b24069eb1106c6ea4
AlexanSimikyan <75497088+AlexanSimikyan@users.noreply.github.com>
2021-01-18 10:19:28 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var canv = document.getElementById('canvas');
+var ctx = canv.getContext('2d');
+function move(x, xd){
+	x = (xd - x) / 2;
+	return x;
+}var xp = 100;
+var yp = 100;
+var ax = 100;
+var ay = 100;
+var bx = 0;
+var by = 400;
+var cx = 400;
+var cy = 400;
+var i = setInterval('game()');
+function game(){
+	let n = Math.round((Math.random()*10))%3;
+	if(n == 0){
+		xp += move(xp, ax);
+		yp += move(yp, ay);
+	}
+	if(n == 1){
+		xp += move(xp, bx);
+		yp += move(yp, by);
+	}
+	if(n == 2){
+		xp += move(xp, cx);
+		yp += move(yp, cy);
+	}
+	ctx.fillRect(xp, yp, 1, 1);
+	ctx.fillRect(ax, ay, 1, 1);
+	ctx.fillRect(bx, by, 1, 1);
+	ctx.fillRect(cx, cy, 1, 1);
+}
+
